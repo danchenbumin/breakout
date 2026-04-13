5069b263ddf80d2981bcd6391722b51c20b3d09e
danchenbumin <3189671037@qq.com>
2026-04-13 17:32:52 +0800

    fix: 解决main.cpp合并冲突，保留重构后的精简版本

--- main.cpp
+++ main.cpp
@@ -53,11 +53,7 @@ int main() {
     CloseWindow();
     return 0;
 } */
-<<<<<<< HEAD
 /* #include "raylib.h"
-=======
-#include "raylib.h"
->>>>>>> b944724345d279076ab6b6a71e2e5e0df864e362
 #include "Ball.h"
 #include "Paddle.h"
 #include "Brick.h"
@@ -203,7 +199,6 @@ int main() {
     // 4. 游戏收尾
     CloseWindow();
     return 0;
-<<<<<<< HEAD
 } */
 // main.cpp 精简后
 #include "raylib.h"
@@ -230,6 +225,4 @@ int main() {
     game.Shutdown();
     CloseWindow();
     return 0;
-=======
->>>>>>> b944724345d279076ab6b6a71e2e5e0df864e362
 }
